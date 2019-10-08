9118a2b4ec284c98d9fba8c94e582628eda52f65
Felix Heck <hi@whotheheck.de>
2019-10-08 15:58:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const { version } = require('./package.json')
 process.on('unhandledRejection', (err) => console.error(err.message))
 
 program
-  .description('Alias the latest deployment to one or multiple `tld`')
+  .description('Alias the latest deployment to one or multiple `fqdn`')
   .version(version)
-  .arguments('<tld...>')
+  .arguments('<fqdn...>')
   .option('-t, --token <token>', 'ZEIT Now token')
   .option('-b, --cleanup-before', 'remove unaliased deployments before')
   .option('-a, --cleanup-afterwards', 'remove unaliased deployments afterwards')
 
 program.parse(process.argv)
 
-const tlds = program.args
+const fqdns = program.args
 const { cleanupBefore, cleanupAfterwards, token } = program
 const tkn = token ? [`--token=${token}`] : []
 const now = (...args) => execa('now', [...tkn, ...args])
@@ -34,15 +34,15 @@ const log = async (fn) => {
 }
 
 ;(async () => {
-  if (!tlds.length) {
-    return console.error(`The argument 'tld' is required.`)
+  if (!fqdns.length) {
+    return console.error(`The argument 'fqdn' is required.`)
   }
   
   cleanupBefore && await log(now('rm', '--safe', '-y', name))
   const src = await getLatestDeployment()
   
-  for (const tld of tlds) {
-    await log(now('alias', src, tld))
+  for (const fqdn of fqdns) {
+    await log(now('alias', src, fqdn))
   }
 
   cleanupAfterwards && await log(now('rm', '--safe', '-y', name))
